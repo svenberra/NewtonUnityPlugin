3238b03502d6f19a58481c2474d3d4782329d46c
JulioJerez <jerezjulio0@gmail.com>
2016-04-24 17:52:58 -0700

    trying to install the first set of callbacks, memory allocations

--- NewtonPlugin/NewtonWorld.cs
+++ NewtonPlugin/NewtonWorld.cs
@@ -6,21 +6,46 @@ using System.Text;
 
 namespace NewtonPlugin
 {
+    // This compiles but I do no think is correct, I do not see any funtion to return the allocated memory
+    class NewtonAllocMemory: SWIGTYPE_p_f_int__p_void
+    {
+        static NewtonAllocMemory()
+        {
+        }
+    }
+
+    class NewtonFreeMemory : SWIGTYPE_p_f_q_const__p_void_int__void
+    {
+        static NewtonFreeMemory()
+        {
+        }
+    }
+
+
     class NewtonWorld
     {
         protected NewtonWorld()
         {
-            pWorld = cpp.NewtonCreate();
-            Debug.Log("Newton World created(" + pWorld.ToString() + ")");
+            // create the static memory alloction glue code. (I am no sur eif this is right)
+            m_alloc = new NewtonAllocMemory();
+            m_free = new NewtonFreeMemory();
+
+            cpp.NewtonSetMemorySystem(m_alloc, m_free);
+
+            m_world = cpp.NewtonCreate();
+            
+            Debug.Log("Newton World created(" + m_world.ToString() + ")");
         }
 
         ~NewtonWorld()
         {
-            cpp.NewtonDestroy(pWorld);
-            Debug.Log("Newton World destroyed(" + pWorld.ToString() + ")");
+            Debug.Log("Newton World destroyed(" + m_world.ToString() + ")");
+            cpp.NewtonDestroy(m_world);
         }
 
-        protected SWIGTYPE_p_NewtonWorld pWorld;
+        protected SWIGTYPE_p_NewtonWorld m_world;
+        static private NewtonAllocMemory m_alloc;
+        static private NewtonFreeMemory m_free;
     }
 }
 
