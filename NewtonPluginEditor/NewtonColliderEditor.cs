7f3153cf9006335ac9da2ec8066a483fbe76b34b
JulioJerez <jerezjulio0@gmail.com>
2016-06-16 08:28:52 -0700

    testing editor script

--- NewtonPluginEditor/NewtonColliderEditor.cs
+++ NewtonPluginEditor/NewtonColliderEditor.cs
@@ -7,16 +7,24 @@ using System.Collections.Generic;
 
 namespace NewtonPlugin
 {
-
     [CustomEditor(typeof(NewtonPlugin.NewtonBoxCollider))]
+    public class NewtonBoxColliderEditor: NewtonColliderEditor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+        }
+    }
+
     public class NewtonColliderEditor : Editor
     {
         public override void OnInspectorGUI()
         {
             UnityEngine.Debug.Log("xxxxxxxxxxxxxxxxxxxxxxxxxx 6");
-            //LevelScript myTarget = (LevelScript)target;
+            NewtonCollider myTarget = (NewtonCollider)target;
             //myTarget.experience = EditorGUILayout.IntField("Experience", myTarget.experience);
             //EditorGUILayout.LabelField("Level", myTarget.Level.ToString());
+            EditorGUILayout.LabelField("testing editor script", "xxxxxxxx");
         }
     }
 }
