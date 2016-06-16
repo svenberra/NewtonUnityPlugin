b5ffc9583b1d48f30e2516a672cb2254c1f5ebfc
JulioJerez <jerezjulio0@gmail.com>
2016-06-16 07:00:37 -0700

    adding the plug in editor dll

---
+++ NewtonPluginEditor/NewtonColliderEditor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEditor;
+using System.Collections.Generic;
+
+
+namespace NewtonPlugin
+{
+
+/*
+
+    [CustomEditor(typeof(NewtonCollider))]
+    public class NewtonColliderEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            //LevelScript myTarget = (LevelScript)target;
+            //myTarget.experience = EditorGUILayout.IntField("Experience", myTarget.experience);
+            //EditorGUILayout.LabelField("Level", myTarget.Level.ToString());
+        }
+    }
+*/
+}
