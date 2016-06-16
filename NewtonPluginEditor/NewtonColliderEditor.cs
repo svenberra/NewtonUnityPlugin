c2546176cc35a954f65db0164bf7eea1d83b19e2
JulioJerez <jerezjulio0@gmail.com>
2016-06-16 08:12:54 -0700

    testing editor script

--- NewtonPluginEditor/NewtonColliderEditor.cs
+++ NewtonPluginEditor/NewtonColliderEditor.cs
@@ -8,7 +8,7 @@ using System.Collections.Generic;
 namespace NewtonPlugin
 {
 
-    [CustomEditor(typeof(NewtonPlugin.NewtonCollider))]
+    [CustomEditor(typeof(NewtonPlugin.NewtonBoxCollider))]
     public class NewtonColliderEditor : Editor
     {
         public override void OnInspectorGUI()
