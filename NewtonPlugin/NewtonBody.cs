ffae20f13dfd7945a0147312135b46c11bf0e9b4
JulioJerez <jerezjulio0@gmail.com>
2016-06-19 08:48:39 -0700

    cleaned up legacy code

--- NewtonPlugin/NewtonBody.cs
+++ NewtonPlugin/NewtonBody.cs
@@ -88,102 +88,22 @@ public class NewtonBodyCollision
 public class NewtonBody : MonoBehaviour
 {
     /*
-            public bool Kinematic = false;
-            public bool KinematicCollidable = false;
-            public float Mass = 1.0f;
+        public bool Kinematic = false;
+        public bool KinematicCollidable = false;
 
-            public Matrix4x4 BodyTransform
-            {
-                get
-                {
-                    Matrix4x4 mat = Matrix4x4.identity;
-                    IntPtr ptrmat = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(Matrix4x4)));
-                    NewtonWrapper.NewtonBodyGetMatrix(m_body, ptrmat);
-                    Marshal.PtrToStructure(ptrmat, mat);
-                    Marshal.FreeHGlobal(ptrmat);
+        static void ApplyForceAndTorque(IntPtr body, float timestep, int threadIndex)
+        {
+            float mass = 0, iXX = 0, iYY = 0, iZZ = 0;
+            NewtonAPI.NewtonBodyGetMassMatrix(body, ref mass, ref iXX, ref iYY, ref iZZ);
 
-                    return mat;
-                }
-
-                set
-                {
-                    NewtonAPI.NewtonBodySetMatrix(pBody, ref value);
-                }
-            }
-
-            public Vector3 GetPosition()
-            {
-                Matrix4x4 mat = Matrix4x4.identity;
-                NewtonAPI.NewtonBodyGetMatrix(pBody, ref mat);
-                return new Vector3(mat.m03, mat.m13, mat.m23);
-            }
-
-            public Quaternion GetRotation()
-            {
-                Matrix4x4 mat = Matrix4x4.identity;
-                NewtonAPI.NewtonBodyGetMatrix(pBody, ref mat);
-                Quaternion q = new Quaternion();
-                q.w = Mathf.Sqrt(Mathf.Max(0, 1 + mat[0, 0] + mat[1, 1] + mat[2, 2])) / 2;
-                q.x = Mathf.Sqrt(Mathf.Max(0, 1 + mat[0, 0] - mat[1, 1] - mat[2, 2])) / 2;
-                q.y = Mathf.Sqrt(Mathf.Max(0, 1 - mat[0, 0] + mat[1, 1] - mat[2, 2])) / 2;
-                q.z = Mathf.Sqrt(Mathf.Max(0, 1 - mat[0, 0] - mat[1, 1] + mat[2, 2])) / 2;
-                q.x *= Mathf.Sign(q.x * (mat[2, 1] - mat[1, 2]));
-                q.y *= Mathf.Sign(q.y * (mat[0, 2] - mat[2, 0]));
-                q.z *= Mathf.Sign(q.z * (mat[1, 0] - mat[0, 1]));
-                return q;
-            }
-
-            public void SetPositionAndRotation(Vector3 position, Quaternion rotation)
-            {
-                Matrix4x4 matrix = Matrix4x4.identity;
-                matrix.SetTRS(position, rotation, Vector3.one);
-                BodyTransform = matrix;
-            }
-
-
-            static void ApplyForceAndTorque(IntPtr body, float timestep, int threadIndex)
-            {
-                float mass = 0, iXX = 0, iYY = 0, iZZ = 0;
-                NewtonAPI.NewtonBodyGetMassMatrix(body, ref mass, ref iXX, ref iYY, ref iZZ);
-
-                Vector3 force = new Vector3(0.0f, -9.8f * mass, 0.0f);
-                NewtonAPI.NewtonBodyAddForce(body, ref force);
-            }
-
-            static void SetTransform(IntPtr pBody, ref Matrix4x4 matrix, int threadIndex)
-            {
-
-                //GCHandle gch = GCHandle.FromIntPtr(NewtonAPI.NewtonBodyGetUserData(pBody));
-
-                //NewtonBody body = (NewtonBody)gch.Target;
-
-                //Matrix4x4 mat = Matrix4x4.identity;
-                //NewtonAPI.NewtonBodyGetMatrix(pBody, ref mat);
-
-                //Quaternion q = new Quaternion();
-                //q.w = Mathf.Sqrt(Mathf.Max(0, 1 + mat[0, 0] + mat[1, 1] + mat[2, 2])) / 2;
-                //q.x = Mathf.Sqrt(Mathf.Max(0, 1 + mat[0, 0] - mat[1, 1] - mat[2, 2])) / 2;
-                //q.y = Mathf.Sqrt(Mathf.Max(0, 1 - mat[0, 0] + mat[1, 1] - mat[2, 2])) / 2;
-                //q.z = Mathf.Sqrt(Mathf.Max(0, 1 - mat[0, 0] - mat[1, 1] + mat[2, 2])) / 2;
-                //q.x *= Mathf.Sign(q.x * (mat[2, 1] - mat[1, 2]));
-                //q.y *= Mathf.Sign(q.y * (mat[0, 2] - mat[2, 0]));
-                //q.z *= Mathf.Sign(q.z * (mat[1, 0] - mat[0, 1]));
-
-                //body.transform.position = new Vector3(mat.m03, mat.m13, mat.m23);
-                //body.transform.rotation = q;
-            }    */
-
-    void Start()
-    {
-        //m_rotBuffer = Marshal.AllocHGlobal(Marshal.SizeOf(typeof (Quaternion)));
-        //m_positBuffer = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(Vector3)));
-    }
+            Vector3 force = new Vector3(0.0f, -9.8f * mass, 0.0f);
+            NewtonAPI.NewtonBodyAddForce(body, ref force);
+        }
+*/
 
     void OnDestroy()
     {
         DestroyRigidBody();
-        //Marshal.FreeHGlobal(m_rotBuffer);
-        //Marshal.FreeHGlobal(m_positBuffer);
     }
 
     // Update is called once per frame
@@ -198,7 +118,6 @@ public class NewtonBody : MonoBehaviour
         transform.rotation = new Quaternion(m_rotationPtr[1], m_rotationPtr[2], m_rotationPtr[3], m_rotationPtr[0]);
         //Debug.Log("yyyy " + transform.rotation[0] + " " + transform.rotation[1] + " " + transform.rotation[2] + " " + transform.rotation[3]);
     }
-
 
     public void InitRigidBody()
     {
