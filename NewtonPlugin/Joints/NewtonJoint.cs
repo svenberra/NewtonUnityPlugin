a6784c210ce7da3092f5633f543cccdb6865811c
JulioJerez <jerezjulio0@gmail.com>
2016-06-23 08:23:17 -0700

    start adding joints.

--- NewtonPlugin/Joints/NewtonJoint.cs
+++ NewtonPlugin/Joints/NewtonJoint.cs
@@ -6,11 +6,10 @@ namespace NewtonPlugin
 
     abstract public class NewtonJoint : MonoBehaviour
     {
-
         abstract public IntPtr CreateJoint();
-
     }
-
+    private NewtonBody m_body;
+    public NewtonBody m_parentBody;
 }
 
 
---
+++ NewtonPlugin/NewtonJoint.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System;
+
+namespace NewtonPlugin
+{
+
+    abstract public class NewtonJoint : MonoBehaviour
+    {
+
+        abstract public IntPtr CreateJoint();
+
+    }
+
+}
+
+
