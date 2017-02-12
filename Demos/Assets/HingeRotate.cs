71eb5d6e6cbe05be4b83d680d6cab31b70510a25
svenberra <sweenie@svenberra.net>
2017-02-12 19:25:38 +0100

    Added HingeActuator scene

---
+++ Demos/Assets/HingeRotate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HingeRotate : MonoBehaviour {
+
+    NewtonActuator na = null;
+    float angle = 0.0f;
+
+    // Use this for initialization
+    void Start () {
+
+        na = GetComponent<NewtonActuator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        angle += 1.0f * Time.deltaTime;
+        if (angle >= 360)
+            angle = angle - 360.0f;
+
+        if (na)
+        {
+            na.TargetAngle = angle;
+        }
+
+		
+	}
+}
+
