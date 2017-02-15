2375427a8d5fea08365840f02e812f0f6c4a3e14
svenberra <sweenie@svenberra.net>
2017-02-15 15:35:13 +0100

    More work on robot, added simple gripper and one more axle

--- Demos/Assets/RobotController.cs
+++ Demos/Assets/RobotController.cs
@@ -4,28 +4,39 @@ using UnityEngine;
 
 public class RobotController : MonoBehaviour {
 
-    NewtonBody baseBody = null;
-    NewtonBody armBody = null;
-    NewtonBody handBody = null;
+    NewtonBody baseNB = null;
+    NewtonBody bodyNB = null;
+    NewtonBody armNB = null;
+    NewtonBody handANB = null;
+    NewtonBody handBNB = null;
     NewtonHingeActuator baseActuator = null;
+    NewtonHingeActuator bodyActuator = null;
     NewtonHingeActuator armActuator = null;
-    NewtonHingeActuator handActuator = null;
+    NewtonSliderActuator handAActuator = null;
+    NewtonSliderActuator handBActuator = null;
 
     private float baseAngle = 0.0f;
+    private float bodyAngle = 0.0f;
     private float armAngle = 0.0f;
-    private float handAngle = 0.0f;
+    private float gripperPos = 0.0f;
 
     // Use this for initialization
     void Start()
     {
-        baseBody = transform.Find("RobotBase").GetComponent<NewtonBody>();
+        baseNB = transform.Find("RobotBase").GetComponent<NewtonBody>();
         baseActuator = transform.Find("RobotBase").GetComponent<NewtonHingeActuator>();
 
-        armBody = transform.Find("RobotArm").GetComponent<NewtonBody>();
+        bodyNB = transform.Find("RobotBody").GetComponent<NewtonBody>();
+        bodyActuator = transform.Find("RobotBody").GetComponent<NewtonHingeActuator>();
+
+        armNB = transform.Find("RobotArm").GetComponent<NewtonBody>();
         armActuator = transform.Find("RobotArm").GetComponent<NewtonHingeActuator>();
 
-        handBody = transform.Find("RobotHand").GetComponent<NewtonBody>();
-        handActuator = transform.Find("RobotHand").GetComponent<NewtonHingeActuator>();
+        handANB = transform.Find("RobotHandA").GetComponent<NewtonBody>();
+        handAActuator = transform.Find("RobotHandA").GetComponent<NewtonSliderActuator>();
+
+        handBNB = transform.Find("RobotHandB").GetComponent<NewtonBody>();
+        handBActuator = transform.Find("RobotHandB").GetComponent<NewtonSliderActuator>();
 
     }
 
@@ -41,27 +52,37 @@ public class RobotController : MonoBehaviour {
     void OnGUI()
     {        
         var oldBaseAngle = baseAngle;
-        baseAngle = GUI.HorizontalSlider(new Rect(25, 25, 100, 30), baseAngle, -180.0F, 180.0F);
+        baseAngle = GUI.HorizontalSlider(new Rect(25, 20, 100, 30), baseAngle, -180.0F, 180.0F);
         if(oldBaseAngle != baseAngle)
         {
             baseActuator.TargetAngle = baseAngle;
-            baseBody.SleepState = false;
+            baseNB.SleepState = false;
+        }
+
+        var oldbodyAngle = bodyAngle;
+        bodyAngle = GUI.HorizontalSlider(new Rect(25, 50, 100, 30), bodyAngle, -45.0F, 45.0F);
+        if (oldbodyAngle != bodyAngle)
+        {
+            bodyActuator.TargetAngle = bodyAngle;
+            bodyNB.SleepState = false;
         }
 
         var oldArmAngle = armAngle;
-        armAngle = GUI.HorizontalSlider(new Rect(25, 25 + 40, 100, 30), armAngle, -180.0F, 180.0F);
+        armAngle = GUI.HorizontalSlider(new Rect(25, 80, 100, 30), armAngle, -60.0F, 60.0F);
         if (oldArmAngle != armAngle)
         {
             armActuator.TargetAngle = armAngle;
-            armBody.SleepState = false;
+            armNB.SleepState = false;
         }
 
-        var oldHandAngle = handAngle;
-        handAngle = GUI.HorizontalSlider(new Rect(25, 25 + 80, 100, 30), handAngle, -180.0F, 180.0F);
-        if (oldHandAngle != handAngle)
+        var oldgripperPos = gripperPos;
+        gripperPos = GUI.HorizontalSlider(new Rect(25, 110, 100, 30), gripperPos, 0.0F, 0.25F);
+        if (oldgripperPos != gripperPos)
         {
-            handActuator.TargetAngle = handAngle;
-            handBody.SleepState = false;
+            handAActuator.TargetPosition = gripperPos;
+            handANB.SleepState = false;
+            handBActuator.TargetPosition = -gripperPos;
+            handBNB.SleepState = false;
         }
 
     }
