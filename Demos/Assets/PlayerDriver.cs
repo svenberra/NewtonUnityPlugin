3fbc8978df8fbfc0378a599fe0223cb7fe4ef938
NewtonDynamics <jerezjulio0@gmail.com>
2017-02-16 14:13:08 -0800

    adding a player driver script to box car, bi I get a hard crash.

---
+++ Demos/Assets/PlayerDriver.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDriver : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        motor = transform.Find("boxcar").GetComponent<NewtonUniversalActuator>();
+        if (motor == null)
+        {
+            Debug.Log("xxxxx motor found");
+        }
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+		
+	}
+
+
+    NewtonUniversalActuator motor = null;
+}
