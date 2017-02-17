fd41758f5771a935a5ceb436325219d76867efb7
NewtonDynamics <jerezjulio0@gmail.com>
2017-02-16 17:09:30 -0800

    adding a simple script to drive box car.

--- Demos/Assets/PlayerDriver.cs
+++ Demos/Assets/PlayerDriver.cs
@@ -8,18 +8,47 @@ public class PlayerDriver : MonoBehaviour {
 	void Start ()
     {
         motor = transform.Find("motor").GetComponent<NewtonBody>();
-//      motorJoint = transform.Find("motor").GetComponent<NewtonUniversalActuator>();
-        if (motor != null)
-        {
-            Debug.Log("xxxxx motor found");
-        }
+        motorJoint = transform.Find("motor").GetComponent<NewtonUniversalActuator>();
     }
 
     // Update is called once per frame
     void Update ()
     {
-		
-	}
+        if (motorJoint != null)
+        {
+            // control forward and backwoar movement
+            if (Input.GetKey(KeyCode.W))
+            {
+                motor.SleepState = false;
+                motorJoint.TargetAngle1 = 1e10f;
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                motor.SleepState = false;
+                motorJoint.TargetAngle1 = -1e10f;
+            }
+            else
+            {
+                motorJoint.TargetAngle1 = motorJoint.GetJointAngle1();
+            }
+
+            // control sterring movement
+            if (Input.GetKey(KeyCode.A))
+            {
+                motor.SleepState = false;
+                motorJoint.TargetAngle0 = 1e10f;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                motor.SleepState = false;
+                motorJoint.TargetAngle0 = -1e10f;
+            }
+            else
+            {
+                motorJoint.TargetAngle0 = motorJoint.GetJointAngle0();
+            }
+        }
+    }
 
     NewtonBody motor = null;
     NewtonUniversalActuator motorJoint = null;
