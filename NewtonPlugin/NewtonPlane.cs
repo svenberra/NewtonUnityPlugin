6eec86fdf9ce9a95a7292e89858a119980cb9bfd
JulioJerez <jerezjulio0@gmail.com>
2017-02-18 05:21:25 -0800

    added planes joint

---
+++ NewtonPlugin/NewtonPlane.cs
@@ -0,0 +1,92 @@
+﻿/*
+* This software is provided 'as-is', without any express or implied
+* warranty. In no event will the authors be held liable for any damages
+* arising from the use of this software.
+* 
+* Permission is granted to anyone to use this software for any purpose,
+* including commercial applications, and to alter it and redistribute it
+* freely, subject to the following restrictions:
+* 
+* 1. The origin of this software must not be misrepresented; you must not
+* claim that you wrote the original software. If you use this software
+* in a product, an acknowledgment in the product documentation would be
+* appreciated but is not required.
+* 
+* 2. Altered source versions must be plainly marked as such, and must not be
+* misrepresented as being the original software.
+* 
+* 3. This notice may not be removed or altered from any source distribution.
+*/
+
+using System;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+
+[AddComponentMenu("Newton Physics/Joints/Plane 3 dof")]
+public class NewtonPlane3DOF: NewtonJoint
+{
+    public override void Create()
+    {
+        NewtonBody child = GetComponent<NewtonBody>();
+        Matrix4x4 localMatrix = Matrix4x4.identity;
+        localMatrix.SetTRS(m_posit, Quaternion.Euler(m_rotation), Vector3.one);
+        IntPtr otherBody = (m_otherBody != null) ? m_otherBody.GetBody().GetBody() : new IntPtr(0);
+
+        Vector3 pin = localMatrix.GetColumn(0);
+        dVector normal = new dVector(pin.x, pin.y, pin.z, 0.0f);
+        dVector posit = new dVector(m_posit.x, m_posit.y, m_posit.z, 1.0f);
+        m_joint = new dNewtonJointPlane3DOF(posit, normal, child.GetBody().GetBody(), otherBody);
+        Stiffness = m_stiffness;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Matrix4x4 bodyMatrix = Matrix4x4.identity;
+        Matrix4x4 localMatrix = Matrix4x4.identity;
+        bodyMatrix.SetTRS(transform.position, transform.rotation, Vector3.one);
+        localMatrix.SetTRS(m_posit, Quaternion.Euler(m_rotation), Vector3.one);
+
+        Gizmos.color = Color.red;
+        Gizmos.matrix = bodyMatrix;
+        Gizmos.DrawRay(m_posit, localMatrix.GetColumn(0) * m_gizmoScale);
+    }
+
+    public Vector3 m_posit = Vector3.zero;
+    public Vector3 m_rotation = Vector3.zero;
+}
+
+[AddComponentMenu("Newton Physics/Joints/Plane 5 dof")]
+public class NewtonPlane5DOF : NewtonJoint
+{
+    public override void Create()
+    {
+        NewtonBody child = GetComponent<NewtonBody>();
+        Matrix4x4 localMatrix = Matrix4x4.identity;
+        localMatrix.SetTRS(m_posit, Quaternion.Euler(m_rotation), Vector3.one);
+        IntPtr otherBody = (m_otherBody != null) ? m_otherBody.GetBody().GetBody() : new IntPtr(0);
+
+        Vector3 pin = localMatrix.GetColumn(0);
+        dVector normal = new dVector(pin.x, pin.y, pin.z, 0.0f);
+        dVector posit = new dVector(m_posit.x, m_posit.y, m_posit.z, 1.0f);
+        m_joint = new dNewtonJointPlane3DOF(posit, normal, child.GetBody().GetBody(), otherBody);
+        Stiffness = m_stiffness;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Matrix4x4 bodyMatrix = Matrix4x4.identity;
+        Matrix4x4 localMatrix = Matrix4x4.identity;
+        bodyMatrix.SetTRS(transform.position, transform.rotation, Vector3.one);
+        localMatrix.SetTRS(m_posit, Quaternion.Euler(m_rotation), Vector3.one);
+
+        Gizmos.color = Color.red;
+        Gizmos.matrix = bodyMatrix;
+        Gizmos.DrawRay(m_posit, localMatrix.GetColumn(0) * m_gizmoScale);
+    }
+
+    public Vector3 m_posit = Vector3.zero;
+    public Vector3 m_rotation = Vector3.zero;
+}
+
+
--- NewtonWrapper/wrapperSdk/dNewtonJointPlane.cpp
+++ NewtonWrapper/wrapperSdk/dNewtonJointPlane.cpp
@@ -30,7 +30,7 @@ dNewtonJointPlane3DOF::dNewtonJointPlane3DOF(const dVector pivot, const dVector 
 	NewtonBody* const netwonBody1 = (NewtonBody*)body1;
 	NewtonBodyGetMatrix(netwonBody0, &bodyMatrix[0][0]);
 
-	CustomPlane3DOF* const joint = new CustomPlane3DOF(pivot, normal, netwonBody0, netwonBody1);
+	CustomPlane3DOF* const joint = new CustomPlane3DOF(bodyMatrix.TransformVector(pivot), bodyMatrix.RotateVector(normal), netwonBody0, netwonBody1);
 	SetJoint(joint);
 }
 
@@ -42,7 +42,7 @@ dNewtonJointPlane5DOF::dNewtonJointPlane5DOF(const dVector pivot, const dVector 
 	NewtonBody* const netwonBody1 = (NewtonBody*)body1;
 	NewtonBodyGetMatrix(netwonBody0, &bodyMatrix[0][0]);
 
-	CustomPlane5DOF* const joint = new CustomPlane5DOF(pivot, normal, netwonBody0, netwonBody1);
+	CustomPlane5DOF* const joint = new CustomPlane5DOF(bodyMatrix.TransformVector(pivot), bodyMatrix.RotateVector(normal), netwonBody0, netwonBody1);
 	SetJoint(joint);
 }
 
