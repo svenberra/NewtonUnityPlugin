1c3073d5a5da18c92887255adb4b1f7ca96411af
JulioJerez <jerezjulio0@gmail.com>
2017-02-20 07:50:13 -0800

    rename first script

--- Demos/Assets/DemoFirstScript.cs
+++ Demos/Assets/DemoFirstScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript: NewtonBodyScript
+public class DemoFirstScript: NewtonBodyScript
 {
 
 	// Use this for initialization
