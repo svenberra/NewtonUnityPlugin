7ff26b8a3fce945bea1406f5e49e29f1edeb13a7
svenberra <sweenie@svenberra.net>
2017-02-21 21:52:15 +0100

    Added button to spawn new bodies at runtime in demo 03

---
+++ Demos/Assets/Scripts/BodySpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodySpawner : MonoBehaviour {
+
+    GameObject reference;
+
+	// Use this for initialization
+	void Start () {
+
+        reference = GameObject.Find("Box1");
+	
+	}
+	
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnGUI()
+    {
+        if(GUI.Button(new Rect(10,10,100,20),"Spawn object"))
+        {
+            var pos = Random.insideUnitSphere * 5 + new Vector3(0,6,0);
+            var rot = Random.rotation;
+            var go = Instantiate(reference, pos, rot);
+            go.GetComponent<NewtonBody>().InitRigidBody();
+        }
+
+    }
+
+
+}
