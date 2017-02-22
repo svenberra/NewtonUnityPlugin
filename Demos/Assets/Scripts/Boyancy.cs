83aed714c95dbde3e751bf2beef2191af1c7361b
svenberra <sweenie@svenberra.net>
2017-02-22 10:37:29 +0100

    Added trigger water demo with "realistic" boyancy ;)

---
+++ Demos/Assets/Scripts/Boyancy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boyancy : NewtonBodyScript
+{
+
+    private Vector3 force;
+    private NewtonBody body;
+
+    void Start()
+    {
+        force = Vector3.zero;
+        body = GetComponent<NewtonBody>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    public override void OnApplyForceAndTorque(float timestep)
+    {
+        body.GetBody().AddForce(new dVector(force.x, force.y, force.z));
+        force = Vector3.zero;
+    }
+
+    override public void OnCollision(NewtonBody otherBody)
+    {
+        if (otherBody.gameObject.tag == "Water")
+        {
+            force = new Vector3(0, 11, 0);
+        }
+    }
+
+}
