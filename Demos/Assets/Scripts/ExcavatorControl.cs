fbd1674077f0c445ca5fade87a66e2b067efa364
NewtonDynamics <jerezjulio0@gmail.com>
2017-04-12 13:15:36 -0700

    works on excavor prototype demo for spetial forum request (WIP)

---
+++ Demos/Assets/Scripts/ExcavatorControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExcavatorControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        // get the element form main body
+        m_baseBody = transform.GetComponent<NewtonBody>();
+        m_baseRotator = transform.GetComponent<NewtonHingeActuator>();
+        
+        //m_baseBody = transform.Find("Excavator").GetComponent<NewtonBody>();
+        //m_baseRotator = transform.Find("Excavator").GetComponent<NewtonHingeActuator>();
+    }
+
+    void OnGUI()
+    {
+        var oldBaseAngle = m_baseAngle;
+        m_baseAngle = GUI.HorizontalSlider(new Rect(25, 20, 250, 30), m_baseAngle, -360.0F, 360.0F);
+        if (oldBaseAngle != m_baseAngle)
+        {
+            m_baseRotator.TargetAngle = m_baseAngle;
+            m_baseBody.SleepState = false;
+        }
+    }
+
+    NewtonBody m_baseBody = null;
+    NewtonHingeActuator m_baseRotator = null;
+
+    private float m_baseAngle = 0.0f;
+}
+
+
+
+
+
