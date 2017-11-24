b14e2ea2919aac92f342da6807fb063df8e70304
svenberra <sweenie@svenberra.net>
2017-11-24 10:59:43 +0100

    Demo 21 added
    Static body and velocity = Conveyor belt

---
+++ Demos/Assets/Scripts/VelocitySet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VelocitySet : MonoBehaviour {
+
+    NewtonBody body = null;
+
+    [Tooltip("Velocity in local space")]
+    public Vector3 Velocity = new Vector3(1,0,0);
+
+	// Use this for initialization
+	void Start () {
+
+        body = GetComponent<NewtonBody>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(body)
+        {
+            body.Velocity = transform.TransformDirection(Velocity);
+        }
+		
+	}
+}
